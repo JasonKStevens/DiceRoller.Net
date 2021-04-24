cb0042049e8501c4bbada5456e48ca9de3fd24a4
Jason Stevens <jstevens@prometheusgroup.com>
2021-04-25 08:59:10 +1200

    Change roller to being cryptographically random.

--- DiceRoller/Dice/RandomNumberGenerator.cs
+++ DiceRoller/Dice/RandomNumberGenerator.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace DiceRoller.Dice
 {
     public class RandomNumberGenerator : IRandomNumberGenerator
     {
-        private static readonly Random rnd = new Random(DateTime.Now.Millisecond);
+        private static readonly RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
 
         public int Next(int maxValue)
         {
-            return rnd.Next(maxValue);
+            var buffer = new byte[sizeof(int)];
+            provider.GetBytes(buffer);
+
+            var randomInt = BitConverter.ToInt32(buffer);
+            var result = Math.Abs(randomInt % maxValue);
+            return result;
         }
     }
 }
