63b64cef23acb1c10f30f7d442a94e5dfaaa20d6
Ed Douglas <ed.douglas@countdown.co.nz>
2024-03-19 20:31:49 +1300

    Effectively a merge of the discord-buttons branch but wiht the latest code too

--- DiscordRollerBot/ICommandProcessor.cs
+++ DiscordRollerBot/ICommandProcessor.cs
@@ -6,7 +6,6 @@ namespace DiscordRollerBot
     public interface ICommandProcessor
     {
         string Prefix { get; }
-        (bool, string) Process(DiscordUserInfo userInfo, string commandText);
         (bool, TypedResult) ProcessTyped(string userId, string commandText);
     }
 }
