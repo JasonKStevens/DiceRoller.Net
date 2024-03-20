e8426546c098f970561f4cfdbafbb2b104ccfa59
Ed Douglas <ed.douglas@countdown.co.nz>
2024-03-20 14:25:41 +1300

    Add UserSettings

---
+++ DiceRollerCmd/SettingResultNode.cs
@@ -0,0 +1,26 @@
+﻿using DiceRoller.Parser;
+
+namespace DiceRollerCmd;
+
+public class SettingResultNode
+{
+    public string Value { get; private set; }
+    public string Breakdown { get; private set; }
+    public TypedResult TypedResult { get; private set; } = new TypedResult();
+
+    public SettingResultNode(string value) : this(value, value, DiceRoller.Parser.TypedResult.NewSimpleResult(value))
+    {
+    }
+
+    public SettingResultNode(string value, string breakdown, TypedResult typedResult = null)
+    {
+        Value = value;
+        Breakdown = breakdown;
+        TypedResult = typedResult;
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
